0305810d04e840204bb6da1dc78b2569b8dad9ff
Arturo Candela <a.candelamolto@edu.gva.es>
2024-05-13 09:52:45 +0200

    feat: Actualizar nombres de variables en la página de inicio
    Se actualizaron los nombres de las variables en la página de inicio para reflejar su propósito real. Ahora se utilizan las variables "Coordenada X" y "Coordenada Y" en lugar de "Nombre" y "Apellido". Esto mejora la claridad y la comprensión del código.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert("Documento Cargado!");
+//Como el script está abajo, se seguro que se ha cargado.
+let X = document.getElementById("X");
+let Y = document.getElementById("Y");
+
+addEventListener("mousemove",(mouseEvent)=>{
+
+    X.innerText = mouseEvent.x;
+    Y.innerText = mouseEvent.y
+
+});
